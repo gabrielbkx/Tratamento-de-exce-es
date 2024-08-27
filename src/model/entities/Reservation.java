c2e02988f7e18de96f062dfde36322597c018b3c
Gabriel <oliveirafrerreira97@hotmail.com>
2024-08-27 20:37:39 -0300

    bad solution for exception tratment

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,59 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkin;
+    private Date checkout;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer romNumber, Date checkin,Date checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+        this.roomNumber = romNumber;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckout() {
+        return checkout;
+    }
+
+    public Date getCheckin() {
+        return checkin;
+    }
+    public long duration() {
+        long diff = checkout.getTime() - checkin.getTime();
+        return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
+    }
+    public void updateDates(Date checkin,Date checkout){
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    @Override
+    public String toString() {
+        return "Room "
+                + roomNumber
+                + ", checkin: "
+                + sdf.format(checkin)
+                + ", checkout: "
+                + sdf.format(checkout)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
